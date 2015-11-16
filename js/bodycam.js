dbe00faa09ae33e5ef560dfa34183fb5d63cb7d9
Timothy Meko <timmeko@gmail.com>
2015-11-16 14:14:21 -0500

    maps working

--- js/bodycam.js
+++ js/bodycam.js
@@ -4,8 +4,8 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1ePutuYZqgK
 
 function init() {
     Tabletop.init( { key: public_spreadsheet_url,
-       callback: showInfo,
-       simpleSheet: true } );
+     callback: showInfo,
+     simpleSheet: true } );
 }
 
 function showInfo(data) {
@@ -83,99 +83,210 @@ function showInfo(data) {
 //map stuff
 
             //Width and height
-            var w = 175;
-            var h = 100;
+            var w = 250;
+            var h = 150;
 
             //Define map projection
             var projection = d3.geo.albersUsa()
-                                   .translate([w/2, h/2])
-                                   .scale([200]);
+            .translate([w/2, h/2])
+            .scale([250]);
 
             //Define path generator
             var path = d3.geo.path()
-                             .projection(projection);
+            .projection(projection);
 
             //Define quantize scale to sort data values into buckets of color
             var color = d3.scale.quantize()
-                                .range(["#1696d2", "#000"]);
+            .range(["#1696d2", "#000"]);
 
             //Create SVG element
             var svg = d3.selectAll(".map-cell")
-                        .append("svg")
-                        .attr("width", w)
-                        .attr("height", h)
-                        .data(data);
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("id", function(d){
+                if (d3.select(this).node().parentNode.parentNode.id == "r2"){ //row2
+                    return "map2";
+                } else if (d3.select(this).node().parentNode.parentNode.id == "r3"){ //row2
+                    return "map3";
+                } else if (d3.select(this).node().parentNode.parentNode.id == "r4"){ //row2
+                    return "map4";
+                } else if (d3.select(this).node().parentNode.parentNode.id == "r5"){ //row2
+                    return "map5";
+                } else if (d3.select(this).node().parentNode.parentNode.id == "r6"){ //row2
+                    return "map6";
+                }
+            })
+            .data(data);
 
             //Load in data
-            // d3.csv("data/State_Emissions.csv", function(data) {
 
-                //Set input domain for color scale
-                color.domain([
-                    d3.min(data, function(d) { return d.passed; }),
-                    d3.max(data, function(d) { return d.passed; })
+            //Set input domain for color scale
+            color.domain([
+                d3.min(data, function(d) { return d.passed; }),
+                d3.max(data, function(d) { return d.passed; })
                 ]);
 
-                //Load in GeoJSON data
-                d3.json("data/us_states.json", function(json) {
+var map2 = d3.select("#map2");
+var map3 = d3.select("#map3");
+var map4 = d3.select("#map4");
+var map5 = d3.select("#map5");
+var map6 = d3.select("#map6");
 
-                    //Merge the ag. data and GeoJSON
-                    //Loop through once for each ag. data value
-                    for (var i = 0; i < data.length; i++) {
+            //Load in GeoJSON data
+            d3.json("data/us_states.json", function(json) {
 
-                        //Grab state name
-                        var dataState = data[i].State;
+                //Merge the data and GeoJSON
+                //Loop through once for each data value
+                for (var i = 0; i < data.length; i++) {
 
-                        //Grab data value
-                        console.log(d3.select(this).node());
-                        var dataValue = data[i].passed;
-// console.log(data);
-                        //Find the corresponding state inside the GeoJSON
-                        for (var j = 0; j < json.features.length; j++) {
+                    //Grab state name
+                    var dataState = data[i].State;
 
-                            var jsonState = json.features[j].properties.name;
-// console.log(dataState, jsonState)
-                            if (dataState == jsonState) {
-// console.log(json.features[j])
-                                //Copy the data value into the JSON
-                                json.features[j].properties.value = dataValue;
+                    //Grab data value
+                    // var dataValue = data[i].passed;
+                    var dataValue = data[i];
 
-                                //Stop looking through the JSON
-                                break;
+                    //Find the corresponding state inside the GeoJSON
+                    for (var j = 0; j < json.features.length; j++) {
 
-                            }
+                        var jsonState = json.features[j].properties.name;
+
+                        if (dataState == jsonState) {
+
+                            //Copy the data value into the JSON
+                            json.features[j].properties.value = dataValue;
+
+                            //Stop looking through the JSON
+                            break;
+
                         }
                     }
+                }
 
-                    //Bind data and create one path per GeoJSON feature
-                    svg.selectAll("path")
-                       .data(json.features)
-                       .enter()
-                       .append("path")
-                       .attr("d", path)
-                       .style("fill", function(d) {
-                            //Get data value
-                            var value = d.properties.value;
+
+                //Bind data and create one path per GeoJSON feature
+                map2.selectAll("path")
+                .data(json.features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .style("fill", function(d) {
+                        //Get data value
+                         console.log(d);
+                         if (d.properties.value){
+                            var value = d.properties.value.passed;
 
                             if (value) {
                                 //If value exists…
-                                return color(value);
+                                return "#1696d2";
                             } else {
                                 //If value is undefined…
-                                return "#ccc";
+                                return "#eee";
                             }
-                       });
+                        }
+                    });
 
-                });
+                //Bind data and create one path per GeoJSON feature
+                map3.selectAll("path")
+                .data(json.features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .style("fill", function(d) {
+                        //Get data value
+                         console.log(d);
+                         if (d.properties.value){
+                            var value = d.properties.value.CreatesRecommendsaStudyGroupPilotProgram;
 
-            // });
+                            if (value) {
+                                //If value exists…
+                                return "#1696d2";
+                            } else {
+                                //If value is undefined…
+                                return "#eee";
+                            }
+                        }
+                    });
+
+                //Bind data and create one path per GeoJSON feature
+                map4.selectAll("path")
+                .data(json.features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .style("fill", function(d) {
+                        //Get data value
+                         console.log(d);
+                         if (d.properties.value){
+                            var value = d.properties.value.AddressesWiretappingPrivacyIssues;
+
+                            if (value) {
+                                //If value exists…
+                                return "#1696d2";
+                            } else {
+                                //If value is undefined…
+                                return "#eee";
+                            }
+                        }
+                    });
+
+                //Bind data and create one path per GeoJSON feature
+                map5.selectAll("path")
+                .data(json.features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .style("fill", function(d) {
+                        //Get data value
+                         console.log(d);
+                         if (d.properties.value){
+                            var value = d.properties.value.DictatesWhereCamerasCanGoBeTurnedOnandOff;
+
+                            if (value) {
+                                //If value exists…
+                                return "#1696d2";
+                            } else {
+                                //If value is undefined…
+                                return "#eee";
+                            }
+                        }
+                    });
+
+                //Bind data and create one path per GeoJSON feature
+                map6.selectAll("path")
+                .data(json.features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .style("fill", function(d) {
+                        //Get data value
+                         console.log(d);
+                         if (d.properties.value){
+                            var value = d.properties.value.PresumptivelyShieldsFootagefromPublicDisclosure;
+
+                            if (value) {
+                                //If value exists…
+                                return "#1696d2";
+                            } else {
+                                //If value is undefined…
+                                return "#eee";
+                            }
+                        }
+                    });
+
+
+            });
 
 
 
 
 
 
-    }
+
+
+}
 
 
 
-    document.write("The published spreadsheet is located at <a target='_new' href='" + public_spreadsheet_url + "'>" + public_spreadsheet_url + "</a>");
+document.write("The published spreadsheet is located at <a target='_new' href='" + public_spreadsheet_url + "'>" + public_spreadsheet_url + "</a>");
